ac83762de5a2a2c0f5ab7c5de1b19841da8af09c
Brett herrell <brett_herrell@yahoo.com>
2021-11-21 18:24:26 -0600

    suggested fix for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,28 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
+var sequelize = new Sequelize(config.database, config.username, config.password, config);
+
 //create connection to the database
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      // host: "localhost",
-      dialect: "mysql",
-      protocol: 'mysql',
-      port: 3306,
-    });
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       // host: "localhost",
+//       dialect: "mysql",
+//       protocol: 'mysql',
+//       port: 3306,
+//     });
+
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
+} else {
+  // the application is executed on the local machine
+  sequelize = new Sequelize("postgres:///my_db");
+}
 
 module.exports = sequelize;
