459885f5ade9113f0340b5f4ff029977e6b16be1
Brett herrell <brett_herrell@yahoo.com>
2021-11-21 18:21:16 -0600

    attempted fix for Heroku

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,9 @@ require("dotenv").config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: "localhost",
+      // host: "localhost",
       dialect: "mysql",
+      protocol: 'mysql',
       port: 3306,
     });
 
